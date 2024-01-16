6aa6e438547d9681b2bbcc2078af09040b468762
Juan Ortiz <juanfortizd@gmail.com>
2024-01-16 16:52:53 -0600

    add list update and delete handlers

--- cmd/gin/main.go
+++ cmd/gin/main.go
@@ -39,10 +39,61 @@ func (h RecipesHandler) CreateRecipe(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "success"})
 
 }
-func (h RecipesHandler) ListRecipes(c *gin.Context)  {}
-func (h RecipesHandler) GetRecipe(c *gin.Context)    {}
-func (h RecipesHandler) UpdateRecipe(c *gin.Context) {}
-func (h RecipesHandler) DeleteRecipe(c *gin.Context) {}
+func (h RecipesHandler) ListRecipes(c *gin.Context)  {
+	list, err := h.store.List()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, list)
+}
+func (h RecipesHandler) GetRecipe(c *gin.Context)    {
+	id := c.Param("id")
+	recipe, err := h.store.Get(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+
+	}
+
+	c.JSON(http.StatusOK, recipe)
+}
+func (h RecipesHandler) UpdateRecipe(c *gin.Context) {
+	var recipe recipes.Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		if err == recipes.NotFoundErr {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := c.Param("id")
+	err := h.store.Update(id, recipe)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+func (h RecipesHandler) DeleteRecipe(c *gin.Context) {
+	id := c.Param("id")
+
+	err := h.store.Remove(id)
+
+	if err != nil {
+		if err == recipes.NotFoundErr {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
 
 func main() {
 	store := recipes.NewMemStore()
